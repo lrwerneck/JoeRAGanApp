66a967dc0fe3f1e8d8e68cfc1e2a1a864c1d88a0
lrwerneck <140346216+lrwerneck@users.noreply.github.com>
2023-09-19 10:01:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,51 +7,17 @@ from langchain.callbacks import StreamlitCallbackHandler
 from langchain.callbacks import get_openai_callback
 import openai
 st.set_page_config(page_title= "The Joe RAGan Experience", page_icon="🤣")
-openai.api_key=st.secrets.OPENAI_API_KEY
 
 
 
 # st.session_state.user = st.sidebar.text_input("Enter your name")
 
-## Lets implement the enter api key function
-# openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-# openai.api_key = openai_api_key
-# if not openai_api_key:
-#     st.info("Please add your OpenAI API key to continue.")
-#     st.stop()
-
-#The process for the Retrieval Agent is the same as before with a few minor adjustments.
-
-#First connect to the hosted weviate stores and create the langchain vectorstores. (Before we inserted the embeddings, this time just connext to an already existing Weviate Store)
-
-#Second Create the Retriever Tools
-
-#Third Create the agent - This will need to be a session_state variable because every user needs a different agent.
-# import weaviate
-# from langchain.embeddings.openai import OpenAIEmbeddings
-# embeddings = OpenAIEmbeddings(openai_api_key=st.secrets.OPENAI_API_KEY)
-
-# if 'client' not in st.session_state:
-#    auth_config = weaviate.AuthApiKey(api_key=st.secrets.WEAVIATE_API_KEY)
-
-#    st.session_state.client = weaviate.Client(
-#         url="https://streamlit-llm-hackathon-vb29y6cc.weaviate.network",
-#         auth_client_secret=auth_config,
-#         additional_headers={
-#             "X-OpenAI-Api-Key": "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"
-#         }
-#     )
-   
-# from langchain.vectorstores import Weaviate
-
-
-# if 'chunk_vectorstore' not in st.session_state:
-#    st.session_state.chunk_vectorstore = Weaviate(st.session_state.client, index_name = "Chunk_Node", text_key = "chunk_body", attributes=['ep_title','ep_link'])
-
-# if 'topic_vectorstore' not in st.session_state:
-#    st.session_state.topic_vectorstore = Weaviate(st.session_state.client, index_name = "Topic_Node", text_key = "topic")
-
-# llm = ChatOpenAI(temperature=0,openai_api_key=st.secrets.OPENAI_API_KEY, model="gpt-3.5-turbo-16k", streaming=True)
+# Lets implement the enter api key function
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+openai.api_key = openai_api_key
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
 
 
 # Connect to and create weaviate vectorstores:
@@ -62,14 +28,14 @@ client = weaviate.Client(
     url="https://streamlit-llm-hackathon-vb29y6cc.weaviate.network",
     auth_client_secret=auth_config,
     additional_headers={
-        "X-OpenAI-Api-Key": "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"
+        "X-OpenAI-Api-Key": openai_api_key
     }
 )
 
 from langchain.vectorstores import Weaviate
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-embeddings = OpenAIEmbeddings(openai_api_key="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")
+embeddings = OpenAIEmbeddings(openai_api_key= openai_api_key)
 chunk_vectorstore = Weaviate(client, index_name = "Chunk_Node", text_key = "chunk_body", attributes=['ep_title','ep_link'])
 topic_vectorstore = Weaviate(client, index_name = "Topic_Node", text_key = "topic")
 
@@ -111,7 +77,7 @@ sys_mes_joe = SystemMessage(content="Do your best to answer the questions. The q
 
 from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
 if 'joe_rogan_agent' not in st.session_state:
-   llm = ChatOpenAI(temperature=0,openai_api_key=st.secrets.OPENAI_API_KEY, model="gpt-3.5-turbo-16k", streaming=True)
+   llm = ChatOpenAI(temperature=0,openai_api_key=openai_api_key, model="gpt-3.5-turbo-16k", streaming=True)
    st.session_state.joe_rogan_agent = create_conversational_retrieval_agent(llm, st.session_state.tools, verbose = False, system_message=sys_mes_joe)
    
 
@@ -128,7 +94,7 @@ if "tokens" not in st.session_state:
 
 if st.sidebar.button("Clear message history"):
     st.session_state.messages = [{"role":"assistant", "content": "What would you like to know?"}]
-    llm = ChatOpenAI(temperature=0,openai_api_key=st.secrets.OPENAI_API_KEY, model="gpt-3.5-turbo-16k", streaming=True)
+    llm = ChatOpenAI(temperature=0,openai_api_key=openai_api_key, model="gpt-3.5-turbo-16k", streaming=True)
     st.session_state.joe_rogan_agent = create_conversational_retrieval_agent(llm, st.session_state.tools, verbose = True, system_message=sys_mes_joe)
 
 for n,msg in enumerate(st.session_state.messages):
@@ -148,4 +114,4 @@ if user_query:
         st.session_state.messages.append({"role": "assistant", "content": response["output"]})
         st.session_state.tokens.append(cb)
         st.write(response["output"])
-        st.write(st.session_state.tokens)
+        st.write(st.session_state.tokens[-1])
